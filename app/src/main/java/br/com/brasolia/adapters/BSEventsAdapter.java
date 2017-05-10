ea9a9bf18cf0b1cab1a8763a4788cdb2dc429c4c
Breno Henrique <brenohff@gmail.com>
2017-05-10 14:05:20 -0300

    - Ajustes no layout da tela; - Opção de curtir evento funcionando;

--- app/src/main/java/br/com/brasolia/adapters/BSEventsAdapter.java
+++ app/src/main/java/br/com/brasolia/adapters/BSEventsAdapter.java
@@ -1,21 +1,37 @@
 package br.com.brasolia.adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.gson.JsonObject;
 
 import java.util.List;
 import java.util.Locale;
 
+import br.com.brasolia.BSLoginActivity;
+import br.com.brasolia.Connectivity.BSConnection;
 import br.com.brasolia.Connectivity.BSImageStorage;
+import br.com.brasolia.Connectivity.BSRequests;
+import br.com.brasolia.Connectivity.BSResponse;
 import br.com.brasolia.R;
+import br.com.brasolia.application.BrasoliaApplication;
 import br.com.brasolia.models.BSEvent;
+import br.com.brasolia.util.AlertUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by cayke on 11/04/17.
@@ -44,36 +60,142 @@ public class BSEventsAdapter extends RecyclerView.Adapter<BSEventViewHolder> {
 
     @Override
     public void onBindViewHolder(final BSEventViewHolder holder, int position) {
+        holder.setEvent(events.get(position));
         holder.bindEvent(events.get(position), choice);
-
     }
 }
 
 class BSEventViewHolder extends RecyclerView.ViewHolder {
 
+    private FrameLayout frameLayout;
+    private ImageButton heart_icon;
     private ImageView cover;
     private TextView title, place, date, price, distance;
     private Context context;
+
+    private boolean liked;
+    private BSEvent event;
 
     public BSEventViewHolder(View itemView) {
         super(itemView);
 
         context = itemView.getContext();
+        heart_icon = (ImageButton) itemView.findViewById(R.id.item_event_heart_icon);
         distance = (TextView) itemView.findViewById(R.id.item_event_distance);
         price = (TextView) itemView.findViewById(R.id.item_event_price);
         title = (TextView) itemView.findViewById(R.id.item_event_title);
         place = (TextView) itemView.findViewById(R.id.item_event_place);
         date = (TextView) itemView.findViewById(R.id.item_event_date);
+        frameLayout = (FrameLayout) itemView.findViewById(R.id.item_event_frameLayout);
 
         cover = (ImageView) itemView.findViewById(R.id.item_event_cover);
+
+        heart_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (BrasoliaApplication.getUser() == null) {
+                    toLikeUserShouldLogin();
+                } else {
+                    liked = !liked;
+                    if (liked)
+                        heart_icon.setImageResource(R.drawable.event_heart_pressed);
+                    else
+                        heart_icon.setImageResource(R.drawable.event_heart);
+
+                    BSRequests requests = BSConnection.createService(BSRequests.class);
+                    Call<JsonObject> call = requests.likeEvent(event.getId(), liked);
+
+                    call.enqueue(new Callback<JsonObject>() {
+                        @Override
+                        public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                            if (response.isSuccessful()) {
+                                BSResponse bsResponse = new BSResponse(response.body());
+                                if (bsResponse.getStatus() == BSResponse.ResponseStatus.BSResponseSuccess) {
+                                    Log.d("likeEvent", "success");
+                                } else {
+                                    Log.d("likeEvent", "server error");
+                                }
+                            } else {
+                                Log.d("likeEvent", "conection failure");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<JsonObject> call, Throwable t) {
+                            Log.d("likeEvent", "conection failure");
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void toLikeUserShouldLogin() {
+        AlertUtil.confirm(context, "Entrar", context.getString(R.string.liked_logout), "Cancelar", "Conectar",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                Intent i = new Intent(context, BSLoginActivity.class);
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                i.putExtra("cameFromApp", true);
+                                context.startActivity(i);
+                                break;
+                        }
+                    }
+                });
+    }
+
+    public void setEvent(BSEvent event){
+        this.event = event;
+    }
+
+    public BSEvent getEvent(){
+        return event;
     }
 
     public void bindEvent(BSEvent event, int choice) {
+
+        //region get if user liked event
+        if (BrasoliaApplication.getUser() != null) {
+            BSRequests requests = BSConnection.createService(BSRequests.class);
+            Call<JsonObject> call = requests.getLikeEvent(getEvent().getId());
+
+            call.enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    if (response.isSuccessful()) {
+                        BSResponse bsResponse = new BSResponse(response.body());
+                        if (bsResponse.getStatus() == BSResponse.ResponseStatus.BSResponseSuccess) {
+                            liked = (boolean) bsResponse.getData();
+                            if (liked)
+                                heart_icon.setBackground(context.getResources().getDrawable(R.drawable.event_heart_pressed));
+                        } else {
+                            liked = false;
+                        }
+                    } else {
+                        liked = false;
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        }
+        //endregion
+
         if (context instanceof AppCompatActivity) {
             DisplayMetrics displaymetrics = new DisplayMetrics();
             ((AppCompatActivity) context).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
             int width = displaymetrics.widthPixels;
             int height = (int) (width * 0.67);
+
+            frameLayout.getLayoutParams().width = width;
+            frameLayout.getLayoutParams().height = height;
 
             switch (choice) {
                 case 1:
